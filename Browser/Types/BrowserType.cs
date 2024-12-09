0a94898a633bd6e1511a200a66140ae473c4f155
Carlos Mena <carlos@carlos.carlos>
2024-12-09 14:55:16 +0100

    BrowserType playright

---
+++ Browser/Types/BrowserType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudBrowserClient.Browser.Types;
+public static class BrowserType {
+    public static readonly string Puppeteer = "";
+    public static readonly string Playright = "playright";
+}

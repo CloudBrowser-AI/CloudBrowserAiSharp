4bd910bb32e4897ec06d5087bca6c9468e95cf58
Carlos Mena <carlos@carlos.carlos>
2024-12-13 16:05:51 +0100

    SessionService BaseAddress

--- Session/SessionService.cs
+++ Session/SessionService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace CloudBrowserClient;
 public class SessionService(string _apiToken) {
     readonly SessionApiClient _client = new();
+    public Uri BaseAddress { get => _client.BaseAddress; set => _client.BaseAddress = value; }
 
     public Task<ListResponse> List(TimeSpan? timeout = null, CancellationToken ct = default) => _client.List(_apiToken, timeout, ct);
     public Task<RemoveResponse> Remove(string label, TimeSpan? timeout = null, CancellationToken ct = default) => _client.Remove(_apiToken, new(label) , timeout, ct);

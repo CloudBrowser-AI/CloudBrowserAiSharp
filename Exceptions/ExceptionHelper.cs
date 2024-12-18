598a5813cacb3020d71bae11079683d16d5144e5
Carlos Mena <carlos@carlos.carlos>
2024-12-18 10:24:01 +0100

    ExceptionHelper

---
+++ Exceptions/ExceptionHelper.cs
@@ -0,0 +1,44 @@
+﻿using CloudBrowserAiSharp.AI.Types.Response;
+using CloudBrowserAiSharp.Browser.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudBrowserAiSharp.Exceptions;
+
+public static class ExceptionHelper {
+    public static void ToException(ResponseStatus status, AIError? aiError) {
+        switch (status) {
+            case ResponseStatus.Succes:
+                return;
+            default:
+            case ResponseStatus.Unknown:
+                throw new UnknownException();
+            case ResponseStatus.AuthorizationError:
+                throw new AuthorizationException();
+            case ResponseStatus.NoSubscription:
+                throw new NoSubscriptionException();
+            case ResponseStatus.NoUnits:
+                throw new NoUnitsException();
+            case ResponseStatus.BrowserLimit:
+                throw new BrowserLimitException();
+            case ResponseStatus.AIError:
+                switch (aiError) {
+                    case AIError.TOO_LONG:
+                        throw new AITooLongException();
+                    case AIError.CONTENT_FLAGGED:
+                        throw new AIContentFlaggedException();
+                    case AIError.INVALID_API_KEY:
+                        throw new AIInvalidApiKeyException();
+                    case AIError.UNKNOWN:
+                    case null:
+                    default:
+                        throw new AIUnknownException();
+                }
+            case ResponseStatus.LabelInUse:
+                throw new LabelInUseException();
+        }
+    }
+}
